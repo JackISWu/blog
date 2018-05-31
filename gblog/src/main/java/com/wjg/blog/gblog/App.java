cb0069fc4da4b2929134e577e2b4f6a6bfc30622
wu_jia <wu_jia@worksap.co.jp>
2018-05-31 15:01:31 +0800

    first commit, spring-boot project base

---
+++ gblog/src/main/java/com/wjg/blog/gblog/App.java
@@ -0,0 +1,13 @@
+package com.wjg.blog.gblog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
